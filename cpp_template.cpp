1c6a799e7a7a5b7ee129d4933fb00bf0b1a5b7a6
Vivek Rai <43493515+Blazer-007@users.noreply.github.com>
2019-10-17 21:51:30 +0530

    Template for CP

---
+++ cpp_template.cpp
@@ -0,0 +1,51 @@
+// Vivek Rai
+// Blazer_007
+
+#include <bits/stdc++.h>
+#define fastio 		ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define mp(i,j)     make_pair(i,j)
+#define inf         0x3f3f3f3f
+#define INF         0x3f3f3f3f3f3f3f3fLL
+#define ll          unsigned long long
+#define pb          push_back
+#define pii         pair<int,int>
+#define vi          vector<int>
+#define vii         vector<pii>
+#define mi          map<int,int>
+#define mii         map<pii,int>
+#define all(a)      (a).begin(),(a).end()
+#define FF           first
+#define SS           second
+#define sz(x)       (int)x.size()
+#define endl        '\n'
+#define hell        1000000007
+#define bit(x, y)   (((x)>>(y))&1)
+#define bctz(x)     (__builtin_ctz(x))
+#define bclz(x)     (__builtin_clz(x))
+#define bclzl(x)    (__builtin_clzll(x))
+#define bctzl(x)    (__builtin_ctzll(x))
+#define bpt(x)      (__builtin_popcount(x))
+#define bptl(x)     (__builtin_popcountll(x))
+#define loop(i,a,b)  for(int i=a;i<b;i++)
+using namespace std;
+
+void solution(){
+	
+}
+
+signed main(){
+//    #ifndef ONLINE_JUDGE
+//    freopen("input.txt","r",stdin);
+//    freopen("output.txt","w",stdout);
+//    #endif
+    fastio;
+    //code to be written here
+    int t;
+    cin>>t;
+    for(int i=1;i<=t;i++){
+    	//cout<<"Case #"<<i<<":";
+    	solution();
+	}       
+    return 0;
+}       
+
