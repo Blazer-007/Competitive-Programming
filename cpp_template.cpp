e129356ef0b503b79971698127b71e60a120b8ce
Vivek Rai <43493515+Blazer-007@users.noreply.github.com>
2019-12-19 23:20:52 +0530

    updated template

--- cpp_template.cpp
+++ cpp_template.cpp
@@ -1,12 +1,16 @@
 // Vivek Rai
 // Blazer_007
 
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("sse2")
+
 #include <bits/stdc++.h>
 #define fastio 		ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 #define mp(i,j)     make_pair(i,j)
 #define inf         0x3f3f3f3f
-#define INF         0x3f3f3f3f3f3f3f3fLL
-#define ll          unsigned long long
+//#define INF         0x3f3f3f3f3f3f3f3fLL
+#define ll          long long
 #define pb          push_back
 #define pii         pair<int,int>
 #define vi          vector<int>
@@ -29,6 +33,43 @@
 #define loop(i,a,b)  for(int i=a;i<b;i++)
 using namespace std;
 
+#define INF 1000000007
+ 
+ll f[100001];
+ 
+ll pow(ll a, ll b, ll MOD)
+{
+ ll x=1,y=a; 
+ while(b > 0)
+ 	{
+ 		if(b%2 == 1)
+ 	{
+ 		x=(x*y);
+ 		if(x>MOD) x%=MOD;
+ 	}
+ 	y = (y*y);
+ 	if(y>MOD) y%=MOD; 
+ 	b /= 2;
+ 	}
+ return x;
+}
+ 
+/* Modular Multiplicative Inverse
+ Using Euler's Theorem
+ a^(phi(m)) = 1 (mod m)
+ a^(-1) = a^(m-2) (mod m) */
+ll InverseEuler(ll n, ll MOD)
+{
+ return pow(n,MOD-2,MOD);
+}
+ 
+ll C(ll n, ll r, ll MOD)
+{
+ 
+ return (f[n]*((InverseEuler(f[r], MOD) * InverseEuler(f[n-r], MOD)) % MOD)) % MOD;
+}
+
+
 void solution(){
 	
 }
@@ -39,13 +80,22 @@ signed main(){
 //    freopen("output.txt","w",stdout);
 //    #endif
     fastio;
+    f[0] = 1;
+	for(int i = 1 ; i <= 100000 ; i++)
+		f[i] = (f[i-1]*i)%INF;
+//    clock_t time_req;
     //code to be written here
-    int t;
+    int t=1;
     cin>>t;
     for(int i=1;i<=t;i++){
     	//cout<<"Case #"<<i<<":";
     	solution();
-	}       
+	}  
+	
+	
+//	time_req = clock() - time_req;
+//    cout<<endl<< "Processor time taken is "<< (double)time_req/CLOCKS_PER_SEC << " seconds" << endl;
+	     
     return 0;
 }       
 
